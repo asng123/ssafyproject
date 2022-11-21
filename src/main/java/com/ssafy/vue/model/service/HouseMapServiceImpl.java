3db076221f4c85b1ad49e499125f06bbd9b86fd6
안승태 <sng_a@naver.com>
2022-11-21 17:27:17 +0900

    [fix] apt API

--- src/main/java/com/ssafy/vue/model/service/HouseMapServiceImpl.java
+++ src/main/java/com/ssafy/vue/model/service/HouseMapServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.vue.model.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,10 +36,10 @@ public class HouseMapServiceImpl implements HouseMapService {
 	public List<HouseInfoDto> getAptInDong(String dong) throws Exception {
 		return sqlSession.getMapper(HouseMapMapper.class).getAptInDong(dong);
 	}
-
-	@Override
-	public List<HouseInfoDto> getAptInDongNDongName(String dong, String dongName) throws Exception {
-		return sqlSession.getMapper(HouseMapMapper.class).getAptInDongNDongName(dong, dongName);
-	}
+//
+//	@Override
+//	public List<HouseInfoDto> getAptInDongNDongName(Map<String, Object> paramMap) throws Exception {
+//		return sqlSession.getMapper(HouseMapMapper.class).getAptInDongNDongName(paramMap);
+//	}
 
 }

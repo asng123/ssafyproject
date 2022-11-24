caa24399b2b48f7a5fd0baa693afb713c39c550c
안승태 <sng_a@naver.com>
2022-11-24 17:51:05 +0900

    [feat] pidx 수정

--- src/main/java/com/ssafy/vue/model/ZipDto.java
+++ src/main/java/com/ssafy/vue/model/ZipDto.java
@@ -31,7 +31,15 @@ public class ZipDto {
 	private String regcode;
 	@ApiModelProperty(value = "요약")
 	private String summary;
+	@ApiModelProperty(value = "사진넘버")
+	private int p_idx;
 	
+	public int getP_idx() {
+		return p_idx;
+	}
+	public void setP_idx(int p_idx) {
+		this.p_idx = p_idx;
+	}
 	public String getSummary() {
 		return summary;
 	}
